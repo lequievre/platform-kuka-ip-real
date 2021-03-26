8491ec1eb1e746dcc8323a09c204d51b0ba53bf4
Laurent LEQUIEVRE <laurent.lequievre@uca.fr>
2021-03-26 15:05:10 +0100

    Changed name of position controller

--- scripts_launcher/kuka_lwr/lanceur_position_service.sh
+++ scripts_launcher/kuka_lwr/lanceur_position_service.sh
@@ -15,7 +15,7 @@ do_start()
 		namespace="kuka_lwr_right"
 	fi
 
-	rosservice call /$namespace/controller_manager/switch_controller "{start_controllers: ['kuka_group_command_controller_fri'], stop_controllers: [], strictness: 2}"
+	rosservice call /$namespace/controller_manager/switch_controller "{start_controllers: ['joint_position_controller'], stop_controllers: [], strictness: 2}"
 }
 
 do_stop()
@@ -28,7 +28,7 @@ do_stop()
 	else
 		namespace="kuka_lwr_right"
 	fi
-	rosservice call /$namespace/controller_manager/switch_controller "{start_controllers: [], stop_controllers: ['kuka_group_command_controller_fri'], strictness: 1}"
+	rosservice call /$namespace/controller_manager/switch_controller "{start_controllers: [], stop_controllers: ['joint_position_controller'], strictness: 1}"
 }
 
 case "$1" in
