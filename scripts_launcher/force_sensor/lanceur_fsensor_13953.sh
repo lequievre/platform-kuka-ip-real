b7869db23297e51beb978d3bc1fa0d59a0006128
lequievre <laurent.lequievre@uca.fr>
2021-03-30 17:53:29 +0200

    Added force sensor net and ros launcher script

---
+++ scripts_launcher/force_sensor/lanceur_fsensor_13953.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Laurent LEQUIEVRE
+# laurent.lequievre@uca.fr
+# UMR 6602 - Institut Pascal
+# Used for the FT Sensor n° 13855
+
+cd /home/kuka/git_projects/platform-kuka-ip-real
+source devel/setup.bash
+export ROS_MASTER_URI=http://kuka-Precision-WorkStation-T7500:11311
+roslaunch force_torque_sensor FTsensor_13953.launch
