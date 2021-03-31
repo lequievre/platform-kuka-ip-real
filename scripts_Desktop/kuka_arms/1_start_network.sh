1c2081b3d730033ff6bdd6978ba4051fb25f3482
Laurent LEQUIEVRE <laurent.lequievre@uca.fr>
2021-03-31 14:47:22 +0200

    Added arms desktop scripts

---
+++ scripts_Desktop/kuka_arms/1_start_network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/kuka/git_project/platform-kuka-ip-real/src/scripts_net/kuka_lwr
+
+./net_kuka_ip.sh start
+
+
+
+
---
+++ scripts_Desktop/kuka_arms/2_start_loading_controllers_both.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/kuka/git_project/platform-kuka-ip-real
+source devel/setup.bash 
+
+
+cd /home/kuka/git_project/platform-kuka-ip-real/src/scripts_launcher/kuka_lwr
+sudo ./lanceur_kuka_arms.sh
+
+
+
